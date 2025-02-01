7881a37add47e348a6328627b018216ceda4f166
Luis Manuel Carballo Gomez <luism.carballog@gmail.com>
2025-02-01 16:45:42 -0600

    Define initial menu for interaction with users

---
+++ App/Controllers/MenuController.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Controllers;
+
+class MenuController {
+    private function menuOptions(): void {
+        echo "\n\n\n";
+        echo "------------------- W E L C O M E -------------------\n";
+        echo "1. Printing Key-Value Pairs\n";
+        echo "2. Sorting Data\n";
+        echo "3. Shopping Cart\n";
+        echo "4. Exit\n";
+    }
+
+    public function run(): void {
+        do {
+            $this->menuOptions();
+            $option = readline("Type option: ");
+
+            switch ($option) {
+                case '1':
+                    echo "\n********* P R I N T I N G *********\n";
+                    break;
+                case '2':
+                    echo "\n********* S O R T I N G *********\n";
+                    break;
+                case '3':
+                    echo "\n********* S H O P P I N G *********\n";
+                    break;
+                case '4':
+                    echo "\n********* C H A O *********\n";
+                    echo "Exiting...\n";
+                    break;
+                default:
+                    echo "\n********* ¡¡ W O O P S !! *********\n";
+                    echo "Invalid option, please try again.\n";
+            }
+
+        } while ($option !== '4');
+    }
+}
